17c9aba8a642eec2300276735342a4bc729d513d
Wilson Cardoso (shaick) <60528183+Shaick@users.noreply.github.com>
2025-04-08 23:47:33 -0300

    submit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-// const fileUpload = require('express-fileupload');
-// const session = require('express-session');
-// const cookieParser = require('cookie-parser');
-// const flash = require('connect-flash');
-// const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+//const mongoose = require('mongoose');
 // mongoose.set('strictQuery', false);
 
 const app = express();
@@ -16,15 +16,15 @@ app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
 app.use(expressLayouts);
 
-/* app.use(cookieParser('CookingBlogSecure'));
+app.use(cookieParser('PFESecure'));
 app.use(session({
-    secret: 'CookingBlogSecretSession',
+    secret: 'PFESecretSession',
     saveUninitialized: true,
     resave: true
   }));
 app.use(flash());
 app.use(fileUpload());
- */
+ 
 
 app.set('layout', './layouts/main');
 app.set('views', './server/views');
